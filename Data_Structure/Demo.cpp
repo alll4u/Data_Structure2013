08af81f4387961a85eb766fa0e1624e48d015302
alll4u <837204976@qq.com>
2017-04-10 17:40:16 +0800

    Stack Applicated in Brackets Match

--- Data_Structure/Demo.cpp
+++ Data_Structure/Demo.cpp
@@ -1,32 +1,70 @@
 #include <iostream>
-#include "LinkNode.h"
+#include <string>
+#include <stdio.h>
+#include "LinkedStack.h"
 using namespace std;
 
-void main() {
-	List<int> lst;
-	int bn = 5;
-	lst.Insert(2, bn);
-	for (int i = 0; i < 10; i++){
-		lst.Insert(i, i);
-	}
-	int a;
-	for (int i = 0; i < 10; i++){
-		lst.getData(i+2, a);
-		
-		cout << "a is " << a << endl;
-	}
-	List<int> lst2 = lst;
-	for (int i = 0; i < 10; i++){
-		lst2.getData(i + 2, a);
-		cout << "address: " << lst.Locate(i + 2) << endl;
-		cout << "address: "<<lst2.Locate(i + 2) << endl;
-		cout << "a is " << a << endl;
-	}
-	lst2.output();
+void PrintMatchedPairs(string exp){
+	LinkedStack<int> s;
+	int length = exp.length();
+	cout << "length is " << length << endl;
+	int j;
+	for (int i = 1; i <= length; i++){
+		if (exp[i - 1] == '(')s.Push(i);
+		else if (exp[i - 1] == ')')
+		{
+			if (s.Pop(j) == true)
+				cout << i << " match " << j << endl;
 
-	List<double> dLst;
-	dLst.input(0.0);
-	dLst.output();
+			else
+				cout << "no match with " << i << endl;
+		}
+
+	}
+	while (s.Pop(j)){
+		cout << "nomatch with " << j << endl;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//int j = 0;
+	//for (int i = 1; i <= length; i++){
+	//	if (exp[i - 1] == '(') s.Push(i);
+	//	else if (exp[i - 1] == ')'){
+	//		if (s.Pop(j) == true)
+	//			cout << j << " and " << i << " match " << endl;
+	//		else cout << j << "no math with " << i << exp[i - 1] << endl;
+	//	}
+	//	else
+	//		cout << exp[i - 1] << endl;
+	//}
+	//while (s.IsEmpty() == false){
+	//	s.Pop(j);
+	//	cout << "no match with " << j << exp[j - 1] << endl;
+	//}
+}
+
+void main() {
+
+	PrintMatchedPairs("((((dd(123d)");
+
 
 	system("pause");
 
